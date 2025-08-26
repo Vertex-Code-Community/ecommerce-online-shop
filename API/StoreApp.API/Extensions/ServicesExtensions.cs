1b02dc38000b238d2667ca87b702cd4e6705ece7
bashivan-lpnu <ivan.bash.pz.2022@lpnu.ua>
2025-08-26 10:55:12 +0300

    Refactor Program.cs and add service extension methods for improved organization and configuration

---
+++ API/StoreApp.API/Extensions/ServicesExtensions.cs
@@ -0,0 +1,167 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using StoreApp.BLL.MapperProvider;
+using StoreApp.BLL.Options;
+using StoreApp.BLL.Security;
+using StoreApp.BLL.Services;
+using StoreApp.BLL.Services.Interfaces;
+using StoreApp.DAL.Data;
+using StoreApp.DAL.Entities;
+using StoreApp.DAL.Repositories;
+using StoreApp.DAL.Repositories.Interfaces;
+using System.Text;
+
+namespace StoreApp.API.Extensions;
+
+
+public static class ServicesExtensions
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services)
+    {
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<ICartItemRepository, CartItemRepository>();
+        services.AddScoped<IReviewRepository, ReviewRepository>();
+
+        services.AddScoped<IJwtProvider, JwtProvider>();
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<ICartItemService, CartItemService>();
+        services.AddScoped<IReviewService, ReviewService>();
+
+        services.AddAutoMapper(typeof(MappingProfile));
+
+        return services;
+    }
+
+    public static IServiceCollection ConfigureCors(this IServiceCollection services, IConfiguration configuration)
+    {
+        var allowedOrigins = configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+        if (allowedOrigins is null || allowedOrigins.Length == 0)
+        {
+            throw new InvalidOperationException("CORS configuration is missing or empty");
+        }
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("Cors", builder =>
+            {
+                builder
+                    .WithOrigins(allowedOrigins)
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+            });
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection ConfigureAuthorization(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        
+        services.AddDbContext<AppDbContext>(options => { options.UseSqlServer(connectionString); });
+
+        services.AddIdentity<UserEntity, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 8;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders()
+            .AddSignInManager();
+
+        return services
+            .AddAuthentication(configuration)
+            .AddAuthorization();
+    }
+
+    public static IServiceCollection ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer", },
+                    }, Array.Empty<string>()
+                },
+            });
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection AddAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtOptions>(configuration.GetSection(nameof(JwtOptions)));
+
+        var jwtOptions = configuration.GetSection(nameof(JwtOptions)).Get<JwtOptions>();
+        if (jwtOptions is null)
+        {
+            throw new InvalidOperationException("Incorrectly configured JwtOptions");
+        }
+        
+        var bytes = Encoding.UTF8.GetBytes(jwtOptions.SecretKey);
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(bytes),
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnChallenge = async context =>
+                    {
+                        Console.WriteLine("JWT Challenge failed:" + context.Error);
+
+                        context.HandleResponse();
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        await context.Response.WriteAsync($"Unauthorized: {context.Error}");
+                    },
+                    OnAuthenticationFailed = async context =>
+                    {
+                        Console.WriteLine("JWT Challenge failed:" + context.Exception);
+
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        await context.Response.WriteAsync("Authentication failed.");
+                    },
+                };
+            });
+
+        return services;
+    }
+}
