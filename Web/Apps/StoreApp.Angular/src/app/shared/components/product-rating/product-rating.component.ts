1e54ce7fd0acbf1038ea067abf8da54b51f5b731
ivan-bash <ivan-bash@vertexcode.dev>
2025-08-28 21:27:51 +0300

    Add product components with styles

---
+++ Web/Apps/StoreApp.Angular/src/app/shared/components/product-rating/product-rating.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-rating',
+  templateUrl: './product-rating.component.html',
+  styleUrls: ['./product-rating.component.css'],
+  standalone: true
+})
+export class ProductRatingComponent {
+  @Input() rating: number = 0;
+  @Input() textSize: string = '1em';
+
+  getStarsArray(): number[] {
+    return Array.from({ length: Math.floor(this.rating) }, (_, i) => i);
+  }
+}
