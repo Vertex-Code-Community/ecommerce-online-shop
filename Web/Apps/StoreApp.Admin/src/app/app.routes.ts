0e73d972ebde1280da41f0eef9ecc82f0e6cebeb
ivan-bash <ivan-bash@vertexcode.dev>
2025-09-03 13:26:40 +0300

    Refactor layout components and update import paths

--- Web/Apps/StoreApp.Admin/src/app/app.routes.ts
+++ Web/Apps/StoreApp.Admin/src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './features/auth/pages/login-page/login-page.component';
 import { guestGuard } from './core/guards/guest-guard';
 import { authGuard } from './core/guards/auth-guard';
-import { AdminLayoutComponent } from './shared/layout/admin-layout.component';
-import { AuthLayoutComponent } from './shared/layout/auth-layout.component';
+import { AdminLayoutComponent } from './shared/layout/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './shared/layout/auth-layout/auth-layout.component';
 import { ProductsComponent } from './features/products/products.component';
 import { ProductFormComponent } from './features/products/product-form/product-form.component';
 
